4b05997d0d5fa42d183d8d20bce0f16aed55b73b
mynamesahu <sambitsahu222@gmail.com>
2017-04-30 21:41:24 +0530

    LOG4J logging feature added to the application which will capture the logs to console output

--- WebAppProject3_SimpleServlet_WithDBConnection_UsingJDBC-SQLQuery/src/java/myServletPackage/ServletwithDBConnection.java
+++ WebAppProject3_SimpleServlet_WithDBConnection_UsingJDBC-SQLQuery/src/java/myServletPackage/ServletwithDBConnection.java
@@ -1,23 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package myServletPackage;
 
-// Loading required libraries
+/*Required libraries*/
 import java.io.*;
-//import java.util.*;
 import javax.servlet.*;
 import javax.servlet.http.*;
 import java.sql.*;
-import static myServletPackage.HelloWebServlet.logger;
 import org.apache.log4j.Logger;
 import org.apache.log4j.PropertyConfigurator;
-/**
- *
- * @author Sambit
- */
+
 public class ServletwithDBConnection extends HttpServlet {
     
     final static Logger logger = Logger.getLogger(ServletwithDBConnection.class);
@@ -64,29 +55,9 @@ public class ServletwithDBConnection extends HttpServlet {
         /* This code is for logging using LOG4J properties file ....END*/
     }    
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        
-    }
+    
 
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
+    
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
@@ -108,80 +79,62 @@ public class ServletwithDBConnection extends HttpServlet {
                         "<body bgcolor=\"#f0f0f0\">\n" +
                         "<h1 align=\"center\">" + title + "</h1>\n"
                      );
-            try{
-                Class.forName(JDBC_DRIVER);                                     // Register JDBC driver
-                conn = DriverManager.getConnection(DB_URL, USERNAME, PASSWORD); // Open a connection
-                
-                //sql = "SELECT * FROM customer";
-                sql = "SELECT cust_id, cust_name, cust_address FROM customer WHERE cust_id=?";
-                customerID = request.getParameter("customerID");
-                logger.info("Information Log"+customerID);
-                
-                //stmt = conn.createStatement();                                // create a Statement object                             
-                //rs = stmt.executeQuery(sql);                                  // Execute SQL query through Statement object
-                
-                ps = conn.prepareStatement(sql);                                // create a PreparedStatement object
-                ps.setString(1, customerID); 
-                
-                rs = ps.executeQuery();                                         // Execute SQL query through PreparedStatement object
-                
-                rs.beforeFirst();
-                // Extract data from result set
-                while(rs.next()){
-                   
-                    //Retrieve by column name
-                    int custId  = rs.getInt("cust_id");
-                    String custName = rs.getString("cust_name");
-                    String custAddress = rs.getString("cust_address");
-                  
-                    //Display values
-                    out.println(" Customer ID: " + custId + "<br>");
-                    out.println(" Customer Name: " + custName + "<br>");
-                    out.println(" Customer Address: " + custAddress + "<br>");
-                    out.println("<br>");
-                }
-                               
-                out.println("</body></html>");
+        try{
+            Class.forName(JDBC_DRIVER);                                     // Register JDBC driver
+            conn = DriverManager.getConnection(DB_URL, USERNAME, PASSWORD); // Open a connection
 
-                // Clean-up environment
-                rs.close();
-                //stmt.close();
-                ps.close();
-                conn.close();
-                
-            }catch(SQLException sqle1){                
-                sqle1.printStackTrace();
-                logger.debug("Debug SQLExeception message : "+ sqle1.getMessage());
-            }catch(Exception e){
-                e.printStackTrace();
-                logger.debug("Debug Exception message : "+ e.getMessage());
-            }
+            //sql = "SELECT * FROM customer";
+            sql = "SELECT cust_id, cust_name, cust_address FROM customer WHERE cust_id=?";
+            customerID = request.getParameter("customerID");
+            logger.info("Information Log"+customerID);
+
+            //stmt = conn.createStatement();                                // create a Statement object                             
+            //rs = stmt.executeQuery(sql);                                  // Execute SQL query through Statement object
+
+            ps = conn.prepareStatement(sql);                                // create a PreparedStatement object
+            ps.setString(1, customerID); 
+
+            rs = ps.executeQuery();                                         // Execute SQL query through PreparedStatement object
+
+            rs.beforeFirst();
+            // Extract data from result set
+            while(rs.next()){
+
+                //Retrieve by column name
+                int custId  = rs.getInt("cust_id");
+                String custName = rs.getString("cust_name");
+                String custAddress = rs.getString("cust_address");
+
+                //Display values
+                out.println(" Customer ID: " + custId + "<br>");
+                out.println(" Customer Name: " + custName + "<br>");
+                out.println(" Customer Address: " + custAddress + "<br>");
+                out.println("<br>");
+            }// End of while
+
+            out.println("</body></html>");
+
+            // Clean-up environment
+            rs.close();
+            //stmt.close();
+            ps.close();
+            conn.close();
+
+        }catch(SQLException sqle1){    // End of try block            
+            sqle1.printStackTrace();
+            logger.debug("Debug SQLExeception message : "+ sqle1.getMessage());
+        }catch(Exception e){
+            e.printStackTrace();
+            logger.debug("Debug Exception message : "+ e.getMessage());
+        }
             
     }// End of doGet()
     
 
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        //processRequest(request, response);
-    }
-
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
+    
     @Override
     public String getServletInfo() {
         return "Short description";
-    }// </editor-fold>
+    }
 
 }
